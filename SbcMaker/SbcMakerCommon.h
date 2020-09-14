90065753b08a2c9ff5e144a04263a625c0e45d3d
nob tominaga <nobuyuki_tominaga@sobal.co.jp>
2020-09-14 14:40:31 +0900

    拡張子用の文字列登録 定義値の修正 コメント付記

--- SbcMaker/SbcMakerCommon.h
+++ SbcMaker/SbcMakerCommon.h
@@ -1,6 +1,8 @@
 #ifndef SBCMAKERCOMMON_H
 #define SBCMAKERCOMMON_H
 
+#include <QString>
+
 // プロジェクト内共通定義
 #ifdef Q_OS_LINUX
 #define SBC_DEFAULT_FILE_PATH	"~/"
@@ -8,20 +10,32 @@
 #define SBC_DEFAULT_FILE_PATH	"c:\\"
 #endif
 
-#define SBC_F_FILE_NAME "%S_%d%dFront.%s"
-#define SBC_B_FILE_NAME "%S_%d%dBack.%s"
-#define SBC_VIEW_F_FILE_NAME "ImgViewFront.%s"
-#define SBC_VIEW_B_FILE_NAME "ImgViewBack.%s"
-#define SBC_PEM_FILE_NAME  "%s_key.pem"
+#define SBC_F_FILE      "Front."        // 社員番号_日付Front.拡張子
+#define SBC_B_FILE      "Back."         // 社員番号_日付Back.拡張子
+#define SBC_VIEW_F_FILE "ImgViewFront." // ImgViewFront.拡張子
+#define SBC_VIEW_B_FILE "ImgViewBack."  // ImgViewBack.拡張子
+#define SBC_PEM_FILE    "_key.pem"      // 社員番号_key.pem
 
+// pem置き場
 #define SBC_PEM_FILE_PATH  "./.security/"
+
+// ViewFile、その他一時ファイル置き場
 #define SBC_TMP_FILE_PATH  "./tmp/"
+
+// 必要なデータ画像置き場
 #define SBC_DATA_FILE_PATH "./.data/"
 
+// FileType
 enum sbcFileType{
   fType_jpg = 0,
   fType_png,
   MAX_FILETYPE,
+};
+// FileType Charactor
+QString strExtension[] = {
+  "jpg",
+  "png",
+  "jpg"
 };
 
 typedef struct {
